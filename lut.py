1937724d693a36f495f7e2ef656833af26ee42b5
Erwin <erwin@adasco.nl>
2020-10-16 21:16:25 +0200

    Working telnet call

--- lut.py
+++ lut.py
@@ -1,15 +1,41 @@
-def calcLut() -> str:
+import tkinter as tk
+import sys
+from tkinter import messagebox 
+import telnetlib
+import time
 
-    import pandas as pd
-    import numpy as np
+def mainLoop():
+    start = time.time()
     
-    df = pd.read_csv(r'test.cube',delim_whitespace=True,header=0,names=['r','g','b'],skiprows=2)
-    df = df.apply(lambda x: np.floor(1023 * x))
-    df = df.astype('Int64')
-    df.to_csv (r'testEW.cube', index = False,sep=' ' ,header=False)
-    return print("File converted")
+    pingTel()
+    
+    end = time.time()
+    print(end - start)
 
+def loadFile():
+        f = open("lut1.cube","r")
+        return f.read()
 
+def pingTel():
+    HOST = "192.168.10.150"
+    #HOST = "towel.blinkenlights.nl"
+    PORT = "9995"
+    
+    print("start")
+    telnetObj=telnetlib.Telnet(HOST,PORT,timeout=2)
+  
+    print("Send LUT")
+
+    #end = "LUT 0:\nLut Kind: 3Dx33x10b\nLut Name: superAwesome"
+
+    lutobj = f"{loadFile()}\n\n"
+    lutobj = bytes(lutobj.encode())
+    print(type(lutobj)) 
+    telnetObj.write(lutobj)
+   
+    telnetObj.interact()    
+    telnetObj.close()
 
 if __name__ == "__main__":
-     calcLut()
+	mainLoop()
+    
