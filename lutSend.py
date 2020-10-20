b3e63c8a4400970a03d63edb265ecf321964ef6e
Erwin <erwin@adasco.nl>
2020-10-20 21:51:42 +0200

    LutSend

---
+++ lutSend.py
@@ -0,0 +1,57 @@
+#Global imports
+import time
+import os
+import telnetlib
+import pandas as pd
+import numpy as np
+
+def loadFile(filePathName):
+        f = open(filePathName,"r")
+        return f.read()
+
+def sendLut(lutInpObj:str) -> str:
+    """Open telnet session and send LUT object
+
+    Args:
+        lutInpObj (str): calculated LUT object
+
+    Returns:
+        str: message if session is closed
+    """
+    HOST = "192.168.10.150" #TODO: refactor to dynamic input
+    PORT = "9995" #TODO: refactor to dynamic input
+    
+    telnetObj=telnetlib.Telnet(HOST,PORT,timeout=2)
+  
+    print("Send LUT")
+
+    #TODO: Extract metadata from input file
+    #start = "LUT DATA 0:"
+    #end = "LUT 0:\nLut Kind: 3Dx33x10b\nLut Name: superAwesome\n"
+    #lutobj = f"{start}\n{lutInpObj}\n\n{end}\n\n"
+    
+    lutobj = lutInpObj
+    print(len(lutobj))
+    lutobj = bytes(lutobj.encode())
+    print("Start write")
+    telnetObj.write(lutobj)
+    print("End write")
+    telnetObj.read_until(b"ACK")
+    telnetObj.close()
+    return "Session closed"
+
+
+def mainLoop():
+    print("Welcome to LUT Convert and Send")
+    inputFile = '/Users/erwinwanders/Documents/Persoonlijk/LUT/lut3d/testEW.cube' #Path to cube file
+    
+    inp = loadFile(inputFile)
+    sendLut(inp)
+    return print("done")
+
+  
+if __name__ == "__main__":
+	mainLoop()
+    
+    
+    
